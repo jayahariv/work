e8434baf7f9a7e593a9bc61d634c8dd461349ef8
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2018-10-06 06:44:40 -0700

    feat: Counter in UI
    * counter starts reducing in UI
    *start and pause subheading add
    * stop and remove timer on zero

--- work/work/CounterViewController.swift
+++ work/work/CounterViewController.swift
@@ -13,10 +13,19 @@ import Repeat
 
 final class CounterViewController: NSViewController {
     // MARK: Properties
+    @IBOutlet private weak var counterTextfield: NSTextField!
+    @IBOutlet private weak var countersubheading: NSTextField!
     @IBOutlet private weak var quitButton: NSButton!
     @IBOutlet private weak var counterContainer: CounterContainerView!
     private var timer: Repeater?
     private var remainingSeconds: Int = 0
+    private struct C {
+        static let LONG_INTERVAL_SEC = 1500
+        static let SHORT_BREAK_SEC = 300
+        static let LONG_BREAK_SEC = 900
+        static let COUNTER_START_LABEL = "Start"
+        static let COUNTER_PAUSE_LABEL = "Pause"
+    }
 
     // MARK: View Lifecycle
     override func viewDidLoad() {
@@ -33,8 +42,10 @@ final class CounterViewController: NSViewController {
     @objc func onToggleTimer() {
         if timer?.state == .executing {
             timer?.pause()
+            countersubheading.stringValue = C.COUNTER_START_LABEL
         } else if timer?.state == .paused {
             timer?.start()
+            countersubheading.stringValue = C.COUNTER_PAUSE_LABEL
         }
     }
 }
@@ -56,7 +67,8 @@ private extension CounterViewController {
     func initializeView() {
         quitButton.keyEquivalent = "q"
         counterContainer.delegate = self
-        remainingSeconds = 1500
+        remainingSeconds = C.LONG_INTERVAL_SEC
+        countersubheading.stringValue = C.COUNTER_START_LABEL
     }
     
     func configureTimer() {
@@ -64,12 +76,27 @@ private extension CounterViewController {
             guard let self = self else {
                 return
             }
+            
+            guard self.remainingSeconds > 0 else {
+                self.resetTimer()
+                return
+            }
             self.remainingSeconds -= 1
-            DispatchQueue.main.async {
-                // update UI
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {
+                    return
+                }
+                let minutesPart = self.remainingSeconds/TimeConstants.MINUTE_IN_SECONDS
+                let secondsPart = self.remainingSeconds%TimeConstants.MINUTE_IN_SECONDS
+                self.counterTextfield.stringValue = String(format: "%02d:%02d", minutesPart, secondsPart)
             }
             print(self.remainingSeconds)
         }
+        timer?.pause()
+    }
+    
+    func resetTimer() {
+        timer = nil
     }
 }
 
---
+++ work/work/TimeConstants.swift
@@ -0,0 +1,15 @@
+//
+/*
+TimeConstants.swift
+Created on: 10/6/18
+
+Abstract:
+ Time Constants will be kept inside this class
+
+*/
+
+import Foundation
+
+struct TimeConstants {
+    static let MINUTE_IN_SECONDS = 60
+}
